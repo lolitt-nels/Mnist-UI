1104f3e18a6801882760c7020be22858437da0f9
zak <imzakbro@gmail.com>
2024-08-30 20:21:24 +0100

    widgets

--- _main_app/settings.py
+++ _main_app/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = f"{BASE_DIR}/staticfiles"
+
+STATICFILES_DIRS = (
+    f"{BASE_DIR}/templates",
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
