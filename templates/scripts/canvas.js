fdb7f265ce46a8b9597345b2bba8922878772ab9
zak <imzakbro@gmail.com>
2024-08-30 21:39:26 +0100

    draw

--- templates/scripts/canvas.js
+++ templates/scripts/canvas.js
@@ -1,24 +1,63 @@
+// script.js
 const canvas = document.getElementById("img");
+
+canvas.width = window.innerWidth * 0.92;
+canvas.height = canvas.width;
+
 const ctx = canvas.getContext("2d");
 
-function drawRect(x, y, w = 1, clr = "#ffffff") {
-  ctx.fillStyle = clr;
-  ctx.fillRect(x, y, w, w);
+//line style
+ctx.strokeStyle = "#ffffff";
+ctx.lineWidth = 40;
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+// Function to start drawing
+function down(e) {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
 }
 
-var pressed = false;
-canvas.addEventListener("mousedown", function () {
-  pressed = true;
+// Function to draw on the canvas
+function move(e) {
+  if (!isDrawing) return;
+
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+}
+
+// Function to stop drawing
+function up() {
+  isDrawing = false;
+}
+
+canvas.addEventListener("mousedown", (e) => {
+  down(e);
 });
-canvas.addEventListener("mouseup", function () {
-  pressed = false;
-});
-canvas.addEventListener("mouseleave", function () {
-  pressed = false;
+canvas.addEventListener("touchstart", (e) => {
+  down(e);
 });
 
-canvas.addEventListener("mousemove", function (e) {
-  if (pressed) {
-    console.log("draw");
-  }
+canvas.addEventListener("mousemove", (e) => {
+  move(e);
+});
+canvas.addEventListener("touchmove", (e) => {
+  move(e);
+});
+
+canvas.addEventListener("mouseup", () => {
+  up();
+});
+canvas.addEventListener("touchend", () => {
+  up();
+});
+// Function to clear the canvas
+document.getElementById("clear").addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
