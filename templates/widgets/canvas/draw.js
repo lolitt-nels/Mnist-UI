50f68face9762a17f12ce6623e3eed4539225987
zak <imzakbro@gmail.com>
2024-08-31 00:33:13 +0100

    smooth canvas

--- templates/widgets/canvas/draw.js
+++ templates/widgets/canvas/draw.js
@@ -1,3 +1,20 @@
+// improved curves
+
+function drawLine(x1, y1, x2, y2) {
+  //line style
+  ctx.strokeStyle = "#ffffff";
+  ctx.lineWidth = canvas.width / 10;
+  ctx.lineCap = "round";
+
+  // Draw a quadratic curve
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.quadraticCurveTo((x1 + x2) / 2, (y1 + y2) / 2, x2, y2);
+  ctx.stroke();
+}
+
+//those are named according to the events
+
 // Function to start drawing
 function down(e) {
   isDrawing = true;
@@ -24,7 +41,8 @@ function move(e) {
     toY = e.touches[0].pageY - canvas.getBoundingClientRect().top;
   }
 
-  ctx.lineTo(toX, toY);
+  //ctx.lineTo(toX, toY);
+  drawLine(lastX, lastY, toX, toY);
 
   ctx.stroke();
 
--- templates/widgets/canvas/vars.js
+++ templates/widgets/canvas/vars.js
@@ -8,9 +8,7 @@ canvas.height = canvas.width;
 
 const ctx = canvas.getContext("2d");
 
-//line style
-ctx.strokeStyle = "#ffffff";
-ctx.lineWidth = canvas.width / 10;
+
 
 let isDrawing = false;
 let lastX = 0;
