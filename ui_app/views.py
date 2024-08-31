44db212045e6ed71f77c7e843c3ea75b542271ac
zak <imzakbro@gmail.com>
2024-08-31 02:42:46 +0100

    ai

--- ui_app/views.py
+++ ui_app/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 from django.http.response import JsonResponse
 
-from .network.preprocess import preprocess
-
-
+from .ai.preprocess import preprocess
+from .ai.MNIST import predict_digit
 
 
 def home(request):
-    if request.method == "POST":        
-        img = preprocess(request.body)                
+    if request.method == "POST":
+        img = preprocess(request.body)
+        prdct = predict_digit(img)
 
-        return JsonResponse({"digit": 9})
+        return JsonResponse({"digit": prdct})
 
     return render(request, "ui_app/home.html", {})
