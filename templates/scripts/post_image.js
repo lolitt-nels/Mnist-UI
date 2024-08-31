493993a1aa520a1516d6f9b95873c3b81e4366f2
zak <imzakbro@gmail.com>
2024-08-31 01:44:43 +0100

    post

--- templates/scripts/post_image.js
+++ templates/scripts/post_image.js
@@ -1,6 +1,10 @@
 //needs the post.js
 
-async function postImage(id) {
-  var digit = await post("http://127.0.0.1:8000", {})
-  console.log(digit)
+async function postImage(canvas_id) {
+  const dataURL = document.getElementById(canvas_id).toDataURL("image/png");
+
+  var digit = await post("http://127.0.0.1:8000", {
+    img: dataURL,
+  });
+  console.log(digit);
 }
--- ui_app/views.py
+++ ui_app/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http.response import JsonResponse
 
+from orjson import loads
+from base64 import b64decode
+from io import BytesIO
+from PIL import Image
 
 def home(request):
     if request.method == "POST":
+        img = loads(request.body).get("img")
+        img = img.split(",")[1]
+
+        # decode the base64
+        img_bytes = b64decode(img)
+
+        # convert the bytes to an img
+        img = Image.open(BytesIO(img_bytes))
+        img.show()
 
         return JsonResponse({"digit": 9})
 
